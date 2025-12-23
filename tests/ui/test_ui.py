55126833a6546075066a311aaebb1e93f39177f7
Yulia Kiiashkina <kiyashkina1207@gmail.com>
2025-12-23 15:30:12 +0200

    Added a test to check the incorrect parcel tracking number as part of individual task

--- tests/ui/test_ui.py
+++ tests/ui/test_ui.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 @pytest.mark.ui
 def test_check_incorrect_username():
@@ -33,6 +34,30 @@ def test_check_incorrect_username():
 
     # Перевіряємо, що назва сторінки така, яку ми очікуємо
     assert driver.title == 'Sign in to GitHub · GitHub'
+
+    # Закриваємо браузер
+    driver.close()
+
+
+# Individual Task
+
+# Тест, що перевіряє пошук посилки на сайті "Нова Пошта"
+@pytest.mark.ui
+def test_check_incorrect_parcel_tracking_number():
+    # Створення об'єкту для керування браузером
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    # Відкриваємо сторінку https://tracking.novaposhta.ua
+    driver.get('https://tracking.novaposhta.ua')
+
+    # Знаходимо поле, в яке будемо вводити неправильний трекінговий номер посилки 
+    tracking_number_elem = driver.find_element(By.ID, 'en')
+
+    # Вводимо неправильний трекінговий номер посилки
+    tracking_number_elem.send_keys('12345000000000')
+
+    # Емулюємо натискання кнопки search
+    tracking_number_elem.send_keys(Keys.ENTER)
 
     # Закриваємо браузер
     driver.close()
